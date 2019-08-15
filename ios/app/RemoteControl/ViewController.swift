b95ad7ab794af4afd4ef25d9b5390566fbb5c23e
Artem Labazin <xxlabaza@gmail.com>
2019-08-15 19:56:03 +0300

    remove submodule entry

---
+++ ios/app/RemoteControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RemoteControl
+//
+//  Created by Artem Labazin on 15/08/2019.
+//  Copyright © 2019 Artem Labazin. All rights reserved.
+//
+
+import UIKit
+import Logging
+import Network
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    let outbound = Outbound.swap
+    
+    let logger = LoggerFactory.create()
+    logger.info("Hello world")
+    
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
