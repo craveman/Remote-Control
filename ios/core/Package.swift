782a6dabb601628a3f961abec25b091a38a2b2f0
Artem Labazin <xxlabaza@gmail.com>
2019-08-22 03:14:51 +0300

    fix libs versions

--- ios/core/Package.swift
+++ ios/core/Package.swift
@@ -33,9 +33,8 @@ let package = Package(
   ],
 
   dependencies: [
-    .package(url: "https://github.com/apple/swift-nio.git", from: "2.6.1"),
-    .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
-    // .package(url: "https://github.com/luoxiu/Schedule", from: "2.0.3"),
+    .package(url: "https://github.com/apple/swift-nio.git", .exact("2.7.0")),
+    .package(url: "https://github.com/apple/swift-nio-extras.git", .exact("1.2.0")),
   ],
 
   targets: [
