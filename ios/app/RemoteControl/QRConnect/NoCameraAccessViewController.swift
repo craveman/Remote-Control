c3d37bfb6c86967ca886ff389995c543367a0677
sergei andreev <andreev-sa@ya.ru>
2019-08-24 01:43:01 +0300

    Added Inspiration controllers; QRcode reader storyboard

---
+++ ios/app/RemoteControl/QRConnect/NoCameraAccessViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NoCameraAccessViewController.swift
+//  RemoteControl
+//
+//  Created by Sergei Andreev on 24/08/2019.
+//  Copyright © 2019 Artem Labazin. All rights reserved.
+//
+
+import UIKit
+
+class NoCameraAccessViewController: UIViewController {
+    @IBAction func toSettings(_ sender: UIButton) {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                print("Settings opened: \(success)") // Prints true
+            })
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
