9a3c976bffee9d28ff2edafac80d4c95ecd57282
Artem Labazin <xxlabaza@gmail.com>
2019-08-30 22:37:06 +0300

    add encoder

--- ios/core/Sources/networking/handlers/OutboundToByteBufferEncoder.swift
+++ ios/core/Sources/networking/handlers/OutboundToByteBufferEncoder.swift
@@ -4,16 +4,34 @@ import NIOExtras
 
 import logging
 
+enum EncodingError: Error {
+
+  case bad
+  case noEncoderFor(Outbound)
+}
 
 final class OutboundToByteBufferEncoder: ChannelOutboundHandler, Loggable {
 
-  private typealias Encoder = (Outbound) -> [UInt8]
-  private static let encoders: [Outbound: Encoder] = [
-      .tick: encodeTick
+  private static func write (to bytes: inout [UInt8], _ string: String) {
+    let stringBytes: [UInt8] = Array(string.utf8)
+    bytes.append(UInt8(stringBytes.count))
+    bytes.append(contentsOf: stringBytes)
+  }
+
+  private typealias Encoder = (Outbound) -> Result<[UInt8], EncodingError>
+  private static let encoders: [UInt8: Encoder] = [
+      0xF1: { response in .success([response.tag, 0x00]) },
+      0x01: encodeSetName
   ]
 
-  private static func encodeTick (response: Outbound) -> [UInt8] {
-    return [0xF1, 0x00]
+  private static func encodeSetName (response: Outbound) -> Result<[UInt8], EncodingError> {
+    guard case let .setName(person, name) = response else {
+      return .failure(.bad)
+    }
+    var result: [UInt8] = [response.tag, 0x00]
+    result.append(person.rawValue)
+    write(to: &result, name)
+    return .success(result)
   }
 
   typealias OutboundIn = Outbound
@@ -22,17 +40,22 @@ final class OutboundToByteBufferEncoder: ChannelOutboundHandler, Loggable {
   public func write (context: ChannelHandlerContext, data: NIOAny, promise: EventLoopPromise<Void>?) {
     let outbound = unwrapOutboundIn(data)
 
-    // guard let encoder = encoders[outbound] else {
-    //   context.fireErrorCaught(.encodingInboundFail("=("))
-    //   return
-    // }
-    let encoder = OutboundToByteBufferEncoder.encodeTick
-    let bytes = encoder(outbound)
+    guard let encoder = OutboundToByteBufferEncoder.encoders[outbound.tag] else {
+      let error = EncodingError.noEncoderFor(outbound)
+      context.fireErrorCaught(error)
+      return
+    }
 
-    var buffer = context.channel.allocator.buffer(capacity: bytes.count)
-    buffer.writeBytes(bytes)
+    switch encoder(outbound) {
+    case let .failure(error):
+      context.fireErrorCaught(error)
+      return
+    case let .success(bytes):
+      var buffer = context.channel.allocator.buffer(capacity: bytes.count)
+      buffer.writeBytes(bytes)
 
-    let out = wrapOutboundOut(buffer)
-    context.write(out, promise: nil)
+      let out = wrapOutboundOut(buffer)
+      context.write(out, promise: nil)
+    }
   }
 }
