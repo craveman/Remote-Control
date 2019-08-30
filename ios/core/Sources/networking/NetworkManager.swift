2f758343862b366607b4414d1e50eb9d43237acd
Artem Labazin <artem.labazin@infobip.com>
2019-08-30 14:37:19 +0300

    enhance NetworkManager protocol

--- ios/core/Sources/networking/NetworkManager.swift
+++ ios/core/Sources/networking/NetworkManager.swift
@@ -1,13 +1,41 @@
 
-public protocol NetworkManager {
+public typealias InboundHandler = (_ message: Inbound) -> Void
+public typealias EventHandler = (_ event: ConnectionEvent) -> Void
 
-  func on (messages handler: @escaping (_ message: Inbound) -> Void)
+public protocol NetworkManagerProtocol {
 
-  func on (events handler: @escaping (_ event: ConnectionEvent) -> Void)
+  func on (messages handler: @escaping InboundHandler)
+
+  func on (events handler: @escaping EventHandler)
 
   func start ()
 
   func stop ()
 
   func send (message: Outbound)
+}
+
+public class NetworkManager: NetworkManagerProtocol {
+
+  public static let shared: NetworkManagerProtocol = NetworkManager()
+
+  public func on (messages handler: @escaping InboundHandler) {
+
+  }
+
+  public func on (events handler: @escaping EventHandler) {
+
+  }
+
+  public func start () {
+
+  }
+
+  public func stop () {
+
+  }
+
+  public func send (message: Outbound) {
+
+  }
 }
