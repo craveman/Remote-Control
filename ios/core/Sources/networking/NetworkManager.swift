973387a4e193ce9251597d93db47a16bca891e4f
Artem Labazin <artem.labazin@infobip.com>
2019-08-30 14:56:35 +0300

    improve NetworkManager protocol

--- ios/core/Sources/networking/NetworkManager.swift
+++ ios/core/Sources/networking/NetworkManager.swift
@@ -1,12 +1,19 @@
 
+import Foundation
+
+
 public typealias InboundHandler = (_ message: Inbound) -> Void
 public typealias EventHandler = (_ event: ConnectionEvent) -> Void
 
 public protocol NetworkManagerProtocol {
 
-  func on (messages handler: @escaping InboundHandler)
+  func on (messages handler: @escaping InboundHandler) -> UUID
 
-  func on (events handler: @escaping EventHandler)
+  func on (events handler: @escaping EventHandler) -> UUID
+
+  func remove (messagesHandler: UUID)
+
+  func remove (eventsHandler: UUID)
 
   func start ()
 
@@ -19,11 +26,21 @@ public class NetworkManager: NetworkManagerProtocol {
 
   public static let shared: NetworkManagerProtocol = NetworkManager()
 
-  public func on (messages handler: @escaping InboundHandler) {
+  public func on (messages handler: @escaping InboundHandler) -> UUID {
+    let uuid = UUID()
+    return uuid
+  }
+
+  public func on (events handler: @escaping EventHandler) -> UUID {
+    let uuid = UUID()
+    return uuid
+  }
+
+  public func remove (messagesHandler: UUID) {
 
   }
 
-  public func on (events handler: @escaping EventHandler) {
+  public func remove (eventsHandler: UUID) {
 
   }
 
