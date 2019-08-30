0dbee5b6882ef822bf2e957f0ed732266b979520
Artem Labazin <artem.labazin@infobip.com>
2019-08-30 14:36:32 +0300

    remove repeatable code

--- ios/core/Sources/logging/Loggable.swift
+++ ios/core/Sources/logging/Loggable.swift
@@ -12,8 +12,6 @@ public extension Loggable {
   }
 
   var log: Logger {
-    let thisType = type(of: self)
-    let name = String(describing: thisType)
-    return LoggerFactory.create(name)
+    return Self.log
   }
 }
