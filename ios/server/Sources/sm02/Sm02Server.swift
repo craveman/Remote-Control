0656158edf8bd886e96dbd8749dcb701ca9306be
Artem Labazin <artem.labazin@infobip.com>
2019-09-26 22:01:04 +0300

    complete server's skeleton

--- ios/server/Sources/sm02/Sm02Server.swift
+++ ios/server/Sources/sm02/Sm02Server.swift
@@ -4,3 +4,24 @@ import Sm02Client
 
 typealias MessagesProcessor = (_ message: Outbound) -> Inbound?
 typealias EventsProcessor = (_ event: ConnectionEvent) -> Void
+
+
+final class Sm02Server {
+
+  let code: [Int]
+  let onMessage: MessagesProcessor
+  let onEvent: EventsProcessor
+
+  init (code: [Int],
+        onMessage: @escaping MessagesProcessor,
+        onEvent: @escaping EventsProcessor
+  ) {
+    self.code = code
+    self.onMessage = onMessage
+    self.onEvent = onEvent
+  }
+
+  func start () {
+    print("Server started")
+  }
+}
--- ios/server/Sources/sm02/main.swift
+++ ios/server/Sources/sm02/main.swift
@@ -8,13 +8,13 @@ guard let wifi = WiFi.info else {
 }
 
 if !Shell.installed("qrencode") {
-  print("WARN: command 'qrencode' is not installed.")
-  print("      It needs for generating QR-codes.")
-  print("      Press 'enter' to install it and continue.")
+  print(" WARN: command 'qrencode' is not installed.")
+  print("       It needs for generating QR-codes.")
+  print("       Press 'enter' to install it and continue.")
   let _ = readLine()
 
   Shell.install("qrencode")
-  print("INFO: 'qrencode' was installed")
+  print(" INFO: 'qrencode' was installed")
 }
 
 let thisServer = RemoteServer(
@@ -35,8 +35,11 @@ switch thisServer.toURL() {
 case let .success(urlString):
   url = urlString
 case let .failure(.encodingJsonError(error)):
+  print("ERROR: JSON encoding server object error.")
+  print("       \(error)")
   exit(1)
 case .failure(.invalidUrlEncoding):
+  print("ERROR: unsuccess URL encoding.")
   exit(1)
 }
 if Shell.exec("qrencode", "-s", "6", "-o", outputFile, url) != 0 {
@@ -44,8 +47,15 @@ if Shell.exec("qrencode", "-s", "6", "-o", outputFile, url) != 0 {
   exit(1)
 }
 
-print("This server object: \(thisServer)")
-print("url: \(url)")
-print("QR-code was writed to \(outputFile)")
+let server = Sm02Server(
+  code: thisServer.code,
+  onMessage: messageHandler,
+  onEvent: eventHandler
+)
+server.start()
+
+print(" INFO: This server object: \(thisServer)")
+print(" INFO: url: \(url)")
+print(" INFO: QR-code was writed to \(outputFile)")
 Shell.exec("open", outputFile)
-print("QR-code was opened in preview app...")
+print(" INFO: QR-code was opened in preview app...")
