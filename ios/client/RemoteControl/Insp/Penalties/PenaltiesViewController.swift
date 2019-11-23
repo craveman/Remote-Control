98b195081413c26c2003bddc22878a2f2a967b9f
Artem Labazin <xxlabaza@gmail.com>
2019-11-23 16:20:53 +0300

    refactor PenaltiesViewController

--- ios/client/RemoteControl/Insp/Penalties/PenaltiesViewController.swift
+++ ios/client/RemoteControl/Insp/Penalties/PenaltiesViewController.swift
@@ -10,66 +10,51 @@ import UIKit
 
 class PenaltiesViewController: UIViewController {
     
-    let storyBoard = "PenaltiesStoryboard"
-    let vcName = "PenaltiesCards"
+  let storyBoard = "PenaltiesStoryboard"
+  let vcName = "PenaltiesCards"
 
-    @IBOutlet weak var penaltiesSubView: UIView!
+  @IBOutlet weak var penaltiesSubView: UIView!
     
-    lazy var penaltiesCtrl: PenaltiesCardsViewController = {
-        let stbrd = UIStoryboard(name: storyBoard, bundle: nil)
-        
-        var vc = stbrd.instantiateViewController(withIdentifier: vcName) as! PenaltiesCardsViewController
-        self.addViewControllerAsChildViewController(childViewController: vc)
-       
-        return vc
-    }()
-    
-    
-    lazy var pCardsCtrl: PenaltiesCardsViewController = {
-        let stbrd = UIStoryboard(name: storyBoard, bundle: nil)
-        
-        var vc = stbrd.instantiateViewController(withIdentifier: vcName) as! PenaltiesCardsViewController
-        
-        self.addViewControllerAsChildViewController(childViewController: vc)
-        return vc
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        updateView()
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    private func updateView() {
-        penaltiesCtrl.view.isHidden = false
-        pCardsCtrl.view.isHidden = true
-        
-        penaltiesCtrl.setType(.basic)
-        pCardsCtrl.setType(.passive)
-    }
-    
-    private func addViewControllerAsChildViewController(childViewController: UIViewController) {
-        
-        addChild(childViewController)
-        
-        penaltiesSubView.addSubview(childViewController.view)
-        childViewController.view.frame = penaltiesSubView.bounds
-        childViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        childViewController.didMove(toParent: self)
-        
-    }
+  lazy var penaltiesCtrl: PenaltiesCardsViewController = {
+    let stbrd = UIStoryboard(name: storyBoard, bundle: nil)
 
-    /*
-    // MARK: - Navigation
+    var vc = stbrd.instantiateViewController(withIdentifier: vcName) as! PenaltiesCardsViewController
+    self.addViewControllerAsChildViewController(childViewController: vc)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    return vc
+  }()
+    
+    
+  lazy var pCardsCtrl: PenaltiesCardsViewController = {
+    let stbrd = UIStoryboard(name: storyBoard, bundle: nil)
 
+    var vc = stbrd.instantiateViewController(withIdentifier: vcName) as! PenaltiesCardsViewController
+
+    self.addViewControllerAsChildViewController(childViewController: vc)
+    return vc
+  }()
+    
+  override func viewDidLoad () {
+    super.viewDidLoad()
+    updateView()
+    // Do any additional setup after loading the view.
+  }
+
+  private func updateView () {
+    penaltiesCtrl.view.isHidden = false
+    pCardsCtrl.view.isHidden = true
+
+    penaltiesCtrl.setType(.basic)
+    pCardsCtrl.setType(.passive)
+  }
+
+  private func addViewControllerAsChildViewController (childViewController: UIViewController) {
+    addChild(childViewController)
+
+    penaltiesSubView.addSubview(childViewController.view)
+    childViewController.view.frame = penaltiesSubView.bounds
+    childViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+    childViewController.didMove(toParent: self)
+  }
 }
