bad3882c04acdb84c4f3e444f76ee845cdaa3c6e
Artem Labazin <xxlabaza@gmail.com>
2019-11-23 15:37:22 +0300

    refactor CommonViewController

--- ios/client/RemoteControl/CommonViewController.swift
+++ ios/client/RemoteControl/CommonViewController.swift
@@ -10,26 +10,13 @@ import UIKit
 
 class CommonViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad () {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
     
-    override func viewWillAppear(_ animated: Bool) {
-        print("CommonViewController::viewWillAppear")
-        super.viewWillAppear(animated)
-        // Do any additional setup after loading the view.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  override func viewWillAppear (_ animated: Bool) {
+    super.viewWillAppear(animated)
+    // Do any additional setup after loading the view.
+  }
 }
